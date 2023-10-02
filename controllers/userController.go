f49a110f8126aef534427e0d40390a9dbd435ac6
Sahil <sahimake1773@gmail.com>
2023-10-02 18:36:09 +0530

    fixed bugs

--- controllers/userController.go
+++ controllers/userController.go
@@ -151,56 +151,44 @@ func GetUsers() gin.HandlerFunc {
 			return
 		}
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		defer cancel()
+		// defer cancel()
 
 		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
 		if err != nil || recordPerPage < 1 {
 			recordPerPage = 10
 		}
-		page, err := strconv.Atoi(c.Query("page"))
-		if err != nil || page < 1 {
+		page, err1 := strconv.Atoi(c.Query("page"))
+		if err1 != nil || page < 1 {
 			page = 1
 		}
 		startIndex := (page - 1) * recordPerPage
-		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+		startIndex, _ = strconv.Atoi(c.Query("startIndex"))
 		matchStage := bson.D{{Key: "$match", Value: bson.D{{}}}}
-		if err != nil {
-			log.Fatal(err)
-		}
+
 		groupStage := bson.D{
 			{Key: "$group", Value: bson.D{
 				{Key: "_id", Value: bson.D{
-					{Key: "_id", Value: "null"},
-				}},
+					{Key: "_id", Value: "null"}}},
 				{Key: "total_count", Value: bson.D{
-					{Key: "$sum", Value: 1},
-				}},
+					{Key: "$sum", Value: 1}}},
 				{Key: "data", Value: bson.D{
-					{Key: "$push", Value: "$$ROOT"},
-				}},
-			}},
-		}
+					{Key: "$push", Value: "$$ROOT"}}}}}}
 		projectStage := bson.D{
 			{Key: "$project", Value: bson.D{
 				{Key: "_id", Value: 0},
 				{Key: "total_count", Value: 1},
 				{Key: "user_items", Value: bson.D{
 					{Key: "$slice", Value: []interface{}{
-						"$data", startIndex, recordPerPage,
-					}},
-				}},
-			}},
-		}
+						"$data", startIndex, recordPerPage}}}}}}}
 		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
-			matchStage, groupStage, projectStage,
-		})
+			matchStage, groupStage, projectStage})
 		defer cancel()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
 		}
 		var allUsers []bson.M
 
-		if err = result.All(ctx, allUsers); err != nil {
+		if err = result.All(ctx, &allUsers); err != nil {
 			log.Fatal(err)
 		}
 		c.JSON(http.StatusOK, allUsers[0])
